6a4958499b23c10b9cedd3838392291fe5dea464
Sampriti Mukherjee <sampriti275@gmail.com>
2024-08-08 23:04:21 +0530

    bug_fixed/scroll

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,12 +11,12 @@ const Home: React.FC = () => {
     <div className="w-full px-4 xl:px-48">
       <Breadcrumb />
       <div className="w-full  flex gap-4">
-        <div className="hidden lg:block h-[calc(100vh-160px)] overflow-y-auto scrollbar-hide md:w-1/4 border">
+        <div className="hidden lg:block h-[calc(100vh-160px)] sm:overflow-y-auto scrollbar-hide md:w-1/4 border">
           <Filter />
         </div>
         <div className="w-full lg:w-3/4 px-2 pt-2">
           <SearchResult />
-          <div className="w-full mt-8 h-[calc(100vh-160px)] overflow-y-auto scrollbar-hide">
+          <div className="w-full mt-8 h-[calc(100vh-160px)] sm:overflow-y-auto scrollbar-hide">
             <PropertyCard />
             <PropertyCard />
           </div>
