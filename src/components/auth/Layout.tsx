706029feb0583eabb63cdf5350318d6dfd141eaf
Sampriti Mukherjee <sampriti275@gmail.com>
2024-08-24 20:24:49 +0530

    feature_added/auth transition changed

--- src/components/auth/Layout.tsx
+++ src/components/auth/Layout.tsx
@@ -15,15 +15,16 @@ const Layout: React.FC<Props> = ({ setOpenAuthModal, openAuthModal }) => {
   const [auth, setAuth] = useState<string>("login");
   return (
     <motion.div
-      initial={{ scale: 0.9, y: "-20%", opacity: 0 }}
+      initial={{ scale: 0.9, x: "-50%", y: "-50%", opacity: 0 }}
       animate={{
         scale: openAuthModal ? 1 : 0.9,
-        y: openAuthModal ? "0%" : "-20%",
+        x: openAuthModal ? "-50%" : "-50%",
+        y: openAuthModal ? "-50%" : "-50%",
         opacity: openAuthModal ? 1 : 0,
       }}
-      exit={{ scale: 0.9, y: "-20%", opacity: 0 }}
+      exit={{ scale: 0.9, x: "-50%", y: "-50%", opacity: 0 }}
       transition={{ type: "spring", stiffness: 300, damping: 30 }}
-      className="w-[420px] bg-white p-4  rounded-lg"
+      className="w-[420px] bg-white p-4 rounded-lg absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
     >
       <div className="w-full flex justify-end">
         <CloseIcon size={6} onClick={handleClose} />
